9b16b7156683e524bcc2417d38028d11f87e43bc
Han Sol Jin <hansol@hansol.ca>
2025-02-09 16:33:46 -0800

    light: Add `panel0-backlight-ex` to the list of backlight devices
    This is used on LGE devices to control backlight during AOD.
    Both `panel0-backlight` and `panel0-backlight-ex` are used; for this
    implementation, setting the brightness for the backlight should set it
    for both nodes.
    
    Change-Id: I341c21ee0b46822bcaef0da1a28cd7d001c3a020

--- light/Devices.cpp
+++ light/Devices.cpp
@@ -18,6 +18,7 @@ namespace light {
 static const std::string kBacklightDevices[] = {
         "backlight",
         "panel0-backlight",
+        "panel0-backlight-ex",
 };
 
 static std::vector<BacklightDevice> getBacklightDevices() {
